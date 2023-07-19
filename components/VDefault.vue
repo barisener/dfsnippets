a4223b0832f4776f1e7460c841b01e3c3832c9e4
Baris Sener <baris@simpliciter.com>
2023-07-19 23:24:52 +0300

    snippets update

---
+++ components/VDefault.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>Component</div>
+</template>
+
+<script setup>
+import { ref, watch, computed } from 'vue';
+import { useI18n } from 'vue-i18n'
+//#region Composables
+const { t } = useI18n()
+//#endregion
+
+//#region Providers
+//#endregion
+
+//#region Injects
+//#endregion
+
+//#region Props
+//#endregion
+
+//#region Emits
+//#endregion
+
+//#region Variables
+//#endregion
+
+//#region Methods
+//#endregion
+
+//#region Computed Properties
+//#endregion
+
+//#region Watchers
+//#endregion
+
+//#region Lifecycle Hooks
+//#endregion
+
+//#region Created
+//#endregion
+</script>
+
+<style lang="scss" scoped></style>
